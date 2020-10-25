29edac078c7ef50e3d5675a43c9070d0e494c348
Sarah Bird <birdsarah@mozilla.com>
2020-10-24 23:48:02 -0500

    test lint fail

--- fta/conftest.py
+++ fta/conftest.py
@@ -6,7 +6,7 @@ from fta.users.tests.factories import UserFactory
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
-    settings.MEDIA_ROOT = tmpdir.strpath
+    settings.MEDIA_ROOT = tmpdir.strpath #afds
 
 
 @pytest.fixture

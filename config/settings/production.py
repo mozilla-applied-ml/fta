745e9a7441f1c5bfdfb87cff7db7ebeca268ff7b
Sarah Bird <birdsarah@mozilla.com>
2020-11-05 12:39:45 -0600

    Don't plan on implementing email.

--- config/settings/production.py
+++ config/settings/production.py
@@ -86,15 +86,18 @@ DEFAULT_FILE_STORAGE = "fta.utils.storages.MediaRootGoogleCloudStorage"
 MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/media/"
 
 # EMAIL
+#
+# NOT DOING EMAIL FOR NOW - Error reporting via dashboard
+#
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="FTA <noreply@example.com>"
-)
+# DEFAULT_FROM_EMAIL = env(
+#    "DJANGO_DEFAULT_FROM_EMAIL", default="FTA <noreply@example.com>"
+# )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
+# SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[FTA]")
+# EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[FTA]")
 
 # ADMIN
 # ------------------------------------------------------------------------------
@@ -124,42 +127,45 @@ EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[FTA]")
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
-        }
-    },
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-        "django.security.DisallowedHost": {
-            "level": "ERROR",
-            "handlers": ["console", "mail_admins"],
-            "propagate": True,
-        },
-    },
-}
+#
+# For now, we'll just rely on google dashboard for reporting
+#
+# LOGGING = {
+#    "version": 1,
+#    "disable_existing_loggers": False,
+#    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+#    "formatters": {
+#        "verbose": {
+#            "format": "%(levelname)s %(asctime)s %(module)s "
+#            "%(process)d %(thread)d %(message)s"
+#        }
+#    },
+#    "handlers": {
+#        "mail_admins": {
+#            "level": "ERROR",
+#            "filters": ["require_debug_false"],
+#            "class": "django.utils.log.AdminEmailHandler",
+#        },
+#        "console": {
+#            "level": "DEBUG",
+#            "class": "logging.StreamHandler",
+#            "formatter": "verbose",
+#        },
+#    },
+#    "root": {"level": "INFO", "handlers": ["console"]},
+#    "loggers": {
+#        "django.request": {
+#            "handlers": ["mail_admins"],
+#            "level": "ERROR",
+#            "propagate": True,
+#        },
+#        "django.security.DisallowedHost": {
+#            "level": "ERROR",
+#            "handlers": ["console", "mail_admins"],
+#            "propagate": True,
+#        },
+#    },
+# }
 
 # Your stuff...
 # ------------------------------------------------------------------------------
